687be6952ee422920ea86fcd7117bb20d1626243
AYGJ <jofre1236@gmail.com>
2022-10-25 18:09:24 -0500

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"], //esto es lo que se configuró
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
