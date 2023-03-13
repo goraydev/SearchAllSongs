00b7386caf3ff9af68c2312b1763577aa6e1112d
AYGJ <jofre1236@gmail.com>
2023-03-12 19:46:10 -0500

    Change HashRouter to BrowserRouter because add a file in public called _redirects

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
